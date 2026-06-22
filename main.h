105d761fddfd42fd14016593d7217e15f92ab835
Jada <jada.lee@g.austincc.edu>
2026-06-21 20:32:51 -0500

    Added functions to header files, as well as documetation

---
+++ main.h
@@ -0,0 +1,13 @@
+/********************************
+* Jada Lee
+* Assignment 4: Simple Stack
+* Purpose: Header file for main.cpp.
+* Includes the Stack ADT for testing.
+********************************/
+
+#ifndef MAIN_H
+define MAIN_H
+
+#include "stack.h"
+
+#endif // MAIN_H
---
+++ stack.h
@@ -0,0 +1,41 @@
+/*********************************
+* Jada Lee
+* Assignment 4: Simple Stack
+* Purpose: Class declaration for the Stack ADT.
+* Implements a simple integer stack using a static
+* array and top index.
+**********************************/
+
+#ifndef STACK_H
+#define STACK_H
+
+#define STACK_SIZE 10
+
+class Stack {
+public:
+
+	/*****************
+	* Constructors/Destructor
+	*****************/
+	Stack()
+	~Stack();
+
+	/****************
+	* Public Methods
+	****************/
+	bool push(int);
+	bool pop(int*);
+	bool peek(int*);
+	bool isEmpty();
+
+private:
+
+	/****************
+	* Attributes
+	****************/
+	int data[STACK_SIZE];
+	int top;
+
+};
+
+#endif // STACK_H
